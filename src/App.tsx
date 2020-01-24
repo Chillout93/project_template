053e0b71337aed1b16658cfb158d2fbda267ac59
Nick Spicer <nick.spicer@mountainwarehouse.com>
2020-01-24 16:01:26 +0000

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
 import * as React from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+const defaultProps = {
+    center: {
+        lat: 59.95,
+        lng: 30.33
+    },
+    zoom: 11
+};
 
 export default class App extends React.Component<{}, {}> {
     state = {
@@ -11,9 +22,18 @@ export default class App extends React.Component<{}, {}> {
 
     render() {
         return (
-            <React.Fragment>
-                <h1>Hello World!</h1>
-            </React.Fragment>
+            <div className="container-fluid">
+                <div id="mapid" style={{ height: 200 }}></div>
+                <Map center={{ lat: 51.505, lng: -0.09 }} zoom={13}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    <Marker position={{ lat: 51.505, lng: -0.09 }}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </Map>
+            </div>
+
         )
     }
 }
